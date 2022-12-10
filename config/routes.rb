5ce629c470dc398abafcd6f96cb34559382779dc
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-12-10 17:01:13 +0300

    add resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :categories
+  resources :post_categories
+  resources :tags
+  resources :post_tags
+  resources :comments
+  resources :posts
+  resources :authors
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
